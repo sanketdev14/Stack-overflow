6ed6f7c7be09b88b981cd2d7fff33fc9c8b2ad3f
Idrees0317 <ahmedidrees0317@gmail.com>
2023-09-12 10:44:09 +0530

    first commit

---
+++ client/src/components/Avatar/Avatar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Avatar = ({ children, backgroundColor,px, py,
+  padding,
+  color,
+  borderRadius,
+   fontSize, cursor}) => {
+  const style ={
+    backgroundColor,
+    padding:`${py} ${px}`,
+    color: color || 'black',
+    borderRadius,
+    textAlign: "center", fontSize,
+    cursor: cursor || null,
+    textDecoration: "none"
+  }
+  return (
+    <div style={style}>
+         { children } 
+    </div>
+  )
+}
+
+export default Avatar
